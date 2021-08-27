e412eea0557deea98ead626847645f19632fc7b6
Charlie Kelly <charliekelly31415@gmail.com>
2021-08-27 17:35:16 -0400

    head component

---
+++ shared-components/head.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function MyHead() {
+  return (
+    <Head>
+      <title>Mostly Landscapes</title>
+      <meta name="description" content="Landscape art project" />
+      <link rel="icon" href="/ck.gif" />
+    </Head>
+  )
+}
