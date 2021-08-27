68c05cd7355c1168da72b7f2b64a1061c27dd4f4
Charlie Kelly <charliekelly31415@gmail.com>
2021-08-27 18:02:23 -0400

    remove missing api ref

--- compositions/image-gallery/index.jsx
+++ compositions/image-gallery/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Image, Transformation } from 'cloudinary-react'
-import { defaultImg } from '../../utils/cloudinaryApi'
 import styles from './image-gallery.module.css'
 
 export default function ImageGallery({ galleryImages, selectThumbnail, currentImageIdx, thumbSize }) {
@@ -17,12 +16,11 @@ export default function ImageGallery({ galleryImages, selectThumbnail, currentIm
       {galleryImages.map((thumb, index) => (
         <Image key={index}
           className={index === currentImageIdx ? selectedImage : thumbnailImage}
-          title={thumb.caption}
-          alt={thumb.description}
+          title={thumb.context.custom.caption}
+          alt=""
           publicId={thumb.public_id}
           onClick={() => selectThumbnail(index)} >
           <Transformation height={thumbSize} width={thumbSize} crop="fill" />
-          <Transformation defaultImage={defaultImg} />
         </Image>
       ))}
     </div>
