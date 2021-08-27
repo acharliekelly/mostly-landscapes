cd745fc2a08c332f69ed60fabf70ca3da74b6974
Charlie Kelly <charliekelly31415@gmail.com>
2021-08-27 18:01:26 -0400

    set story paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,9 @@
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../compositions/**/*.stories.jsx",
+    "../shared-components/**/*.stories.jsx"
   ],
   "addons": [
     "@storybook/addon-links",
